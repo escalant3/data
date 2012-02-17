bb681cf6eb6650e4d8ae7ab08f0064d7447434d9
Diego Muñoz Escalante <escalant3@gmail.com>
2012-02-16 20:21:52 -0500

    Added basic documentation to the adapter

--- packages/ember-data/lib/adapters/tastypie-adapter.js
+++ packages/ember-data/lib/adapters/tastypie-adapter.js
@@ -1,10 +1,40 @@
 require("ember-data/core");
 require("ember-data/system/adapters");
 
+/*
+ * The DjangoTastypie Adapter extends the default RESTAdapter
+ * from Ember.js in order to work with a REST interface provided
+ * by django-tastypie.
+ *
+ * Some details of implementation of that library that differ
+ * from the Rails oriented RESTAdapter are:
+ * - Sent data must be stringified.
+ * - Sent data must include a CONTENT_TYPE='application/json' header
+ * - There are no plurals for collection of objects
+ * - Returned objects are not inside a json[root] object
+ * - Bulk Commits are not supported by default
+ *
+ */
+
 DS.DjangoTastypieAdapter = DS.RESTAdapter.extend({
+  /* 
+   * This is the default Tastypie url found in the documentation.
+   * You may change it if necessary when creating the adapter
+   */
   tastypieApiUrl: "api/v1/",
+
+  
+  /*
+   * Bulk commits are not supported at this time by the adapter.
+   * Changing this setting will not work
+   */
   bulkCommit: false,
 
+  
+  /* 
+   * Create a record in the Django server. POST actions must
+   * be enabled in the Resource
+   */
   createRecord: function(store, type, model) {
     var root = this.rootForType(type);
 
@@ -17,7 +47,11 @@ DS.DjangoTastypieAdapter = DS.RESTAdapter.extend({
       }
     });
   },
-    
+ 
+  /* 
+   * Edit a record in the Django server. PUT actions must
+   * be enabled in the Resource
+   */
   updateRecord: function(store, type, model) {
     var id = get(model, 'id');
     var root = this.rootForType(type);
@@ -34,6 +68,10 @@ DS.DjangoTastypieAdapter = DS.RESTAdapter.extend({
     });
   },
 
+  /* 
+   * Delete a record in the Django server. DELETE actions
+   * must be enabled in the Resource
+   */
   deleteRecord: function(store, type, model) {
     var id = get(model, 'id');
     var root = this.rootForType(type);
@@ -53,7 +91,6 @@ DS.DjangoTastypieAdapter = DS.RESTAdapter.extend({
 
     this.ajax(url, "GET", {
       success: function(json) {
-        console.log(json);
         store.load(type, json);
       }
     });

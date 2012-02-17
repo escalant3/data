d75054ab7673a4a7d4e64cb35e06bfbb9b9891b2
Diego Muñoz Escalante <escalant3@gmail.com>
2012-02-16 20:10:14 -0500

    Added CRUD operations to Django Adapter

--- packages/ember-data/lib/adapters/tastypie-adapter.js
+++ packages/ember-data/lib/adapters/tastypie-adapter.js
@@ -2,7 +2,50 @@ require("ember-data/core");
 require("ember-data/system/adapters");
 
 DS.DjangoTastypieAdapter = DS.RESTAdapter.extend({
-  tastypieApiUrl: null,
+  tastypieApiUrl: "api/v1/",
+  bulkCommit: false,
+
+  createRecord: function(store, type, model) {
+    var root = this.rootForType(type);
+
+    var data = JSON.stringify(get(model, 'data'));
+
+    this.ajax(root, "POST", {
+      data: data,
+      success: function(json) {
+        store.didCreateRecord(model, json);
+      }
+    });
+  },
+    
+  updateRecord: function(store, type, model) {
+    var id = get(model, 'id');
+    var root = this.rootForType(type);
+
+    var data = JSON.stringify(get(model, 'data'));
+
+    var url = [root, id].join("/");
+
+    this.ajax(url, "PUT", {
+      data: data,
+      success: function(json) {
+        store.didUpdateRecord(model, json[root]);
+      }
+    });
+  },
+
+  deleteRecord: function(store, type, model) {
+    var id = get(model, 'id');
+    var root = this.rootForType(type);
+
+    var url = [root, id].join("/");
+
+    this.ajax(url, "DELETE", {
+      success: function(json) {
+        store.didDeleteRecord(model);
+      }
+    });
+  },
 
   find: function(store, type, id) {
     var root = this.rootForType(type);
@@ -10,6 +53,7 @@ DS.DjangoTastypieAdapter = DS.RESTAdapter.extend({
 
     this.ajax(url, "GET", {
       success: function(json) {
+        console.log(json);
         store.load(type, json);
       }
     });
@@ -50,9 +94,11 @@ DS.DjangoTastypieAdapter = DS.RESTAdapter.extend({
     hash.url = this.getTastypieUrl(url);
     hash.type = type;
     hash.dataType = "json";
-
+    hash.contentType = 'application/json';
     jQuery.ajax(hash);
+  },
+
+  pluralize: function(name) {
+    return name;
   }
 });
-
-

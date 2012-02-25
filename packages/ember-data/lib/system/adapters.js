ab8fe240b815195da2aa5b144c949cdfa8fc9dd9
Diego Muñoz Escalante <escalant3@gmail.com>
2012-02-25 08:54:48 -0500

    Added load of REST adapters to minispade

--- packages/ember-data/lib/system/adapters.js
+++ packages/ember-data/lib/system/adapters.js
@@ -39,3 +39,5 @@ DS.Adapter = Ember.Object.extend({
 });
 
 require('ember-data/adapters/fixture_adapter');
+require('ember-data/adapters/rest_adapter');
+require('ember-data/adapters/tastypie-adapter');
